2d3e3a2de11a0290c85d0be211d7057efe34d3ee
JorgeA3 <0246911@up.edu.mx>
2025-10-09 20:47:27 -0600

    build

---
+++ linux/build/CMakeFiles/caesar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/caesar.dir/caesar.c.o"
+  "CMakeFiles/caesar.dir/caesar.c.o.d"
+  "libcaesar.a"
+  "libcaesar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/caesar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
